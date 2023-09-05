9eda6667f88698f9328bdfcdc274896f3c9ec100
VitorMiranda11 <vitormiranda4321@outlook.com>
2023-09-05 19:55:39 -0300

    criando objeto para poder listar na pagina de produtos

---
+++ src/components/listaProdutos.js
@@ -0,0 +1,18 @@
+
+export const listaProdutos = [
+    {
+        id: 1,
+        nome: 'Teclado',
+        preco: 400
+    },
+    {
+        id: 2,
+        nome: 'Mouse',
+        preco: 200
+    },
+    {
+        id: 3,
+        nome: 'Monitor',
+        preco: 800
+    }
+]
